d25989c9c2a3243cb30fe9a54d52eacd4d818ed8
Dong Cheng <dongcheng01@chinatopcredit.com>
2015-07-16 11:37:24 +0800

    8

--- src/com/ztx/qa/learnSpring6/LogginAspect.java
+++ src/com/ztx/qa/learnSpring6/LogginAspect.java
@@ -3,6 +3,7 @@ package com.ztx.qa.learnSpring6;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @Component
 @Aspect
+@Order(2)
 public class LogginAspect {
     @Before("execution(* com.ztx.qa.learnSpring6.Calculator.*(..))")
     public void beforeMethod(JoinPoint joinPoint) {
---
+++ src/com/ztx/qa/learnSpring6/ValidationAspect.java
@@ -0,0 +1,22 @@
+package com.ztx.qa.learnSpring6;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * Created by s016374 on 15/7/16.
+ */
+@Component
+@Aspect
+@Order(1)
+public class ValidationAspect {
+    @Before("execution(* com.ztx.qa.learnSpring6.Calculator.*(..))")
+    public void validationMethod(JoinPoint joinPoint) {
+        System.out.println("validationMethod...methodName:[" + joinPoint.getSignature().getName() + "],args:" + Arrays.asList(joinPoint.getArgs()));
+    }
+}
